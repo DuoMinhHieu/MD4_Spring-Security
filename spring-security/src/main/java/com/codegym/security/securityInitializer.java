c60fa86527f4b39c79782c50dc880c6c969fbf88
DuoMinhHieu <dmh6774@gmail.com>
2024-05-30 08:40:21 +0700

    main

---
+++ spring-security/src/main/java/com/codegym/security/securityInitializer.java
@@ -0,0 +1,6 @@
+package com.codegym.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
